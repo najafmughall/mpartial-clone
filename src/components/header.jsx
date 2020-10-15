923c45b7ba8e00b380ac179c96563a7ad4eeebb6
Najaf <najafali2002@gmail.com>
2020-10-16 03:39:20 +0500

    Header updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,62 @@
 import React, { Component } from "react";
+import logo from "../images/logo.png";
+import video from "../images/homepage.mp4";
 
 class Header extends Component {
   render() {
     return (
       <React.Fragment>
         <header>
-          <a href="#">
-            <img src="logo.png" alt="Logo" />
-          </a>
-          <a href="#">Ground-Truth Data</a>
-          <a href="#">How It Works</a>
-          <a href="#">Fee Structure</a>
-          <a href="#">Example Devliverables</a>
-          <a href="#">Submission Portal</a>
-          <a href="#">Contact Us</a>
-          <button>Sign in</button>
+          <div>
+            <video className="videoTag" autoPlay loop muted>
+              <source src={video} type="video/mp4" />
+            </video>
+          </div>
+          <nav className="navbar navbar-expand-md">
+            <div className="container">
+              <a className="navbar-brand" href="#">
+                <img src={logo} alt="Logo" width="217" />
+              </a>
+
+              <div className="collaspe navbar-collaspe">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link " href="#">
+                      Ground-Truth Data
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      How It Works
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      Fee Structure
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      Example Devliverables
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      Submission Portal
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      Contact Us
+                    </a>
+                  </li>
+                  <li className="nav-item active">
+                    <button className="btn-signIn">Sign in</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </header>
       </React.Fragment>
     );
