ec451afba812a2d26b1e4c60948f35141e1a5000
Najaf <najafali2002@gmail.com>
2020-10-17 00:56:44 +0500

    How It Works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./header.css";
 import "./ground.css";
+import "./howitworks.css";
 
 import Header from "./components/header";
 import GroundTruth from "./components/ground-truth";
+import HowItWorks from "./components/howitworks";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <GroundTruth />
+      <HowItWorks />
     </div>
   );
 }
---
+++ src/components/howitworks.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class HowItWorks extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className="section_text">How It Works</h1>
+            </div>
+          </div>
+          <div className="data">
+            <div className="row marg">
+              <div className="col-md-4 col-sm-12">
+                <div className="row marg">
+                  <div className="col-md-4 col-sm-12 data-number">1</div>
+                  <div className="col-md-8 col-sm-12 data-text">
+                    <a href="#">
+                      Perform pre-mitigation and post-mitigation scans with a
+                      Matterport Pro Series camera.
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 col-sm-12">
+                <div className="row marg">
+                  <div className="col-md-4 col-sm-12 data-number">2</div>
+                  <div className="col-md-8 col-sm-12 data-text">
+                    <span>
+                      Submit the Matterport scans via the mpartial portal and
+                      then go back to what you do great.
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 col-sm-12">
+                <div className="row marg">
+                  <div className="col-md-4 col-sm-12 data-number">3</div>
+                  <div className="col-md-8 col-sm-12 data-text">
+                    <span>
+                      Receive a well-formatted, fully documented Xactimate PDF,
+                      ESX & Matterport TrueSketch PLUS SKX.
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="try_btn">
+            <a href="#">
+              <button className="btn-signIn">Get Started</button>
+            </a>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HowItWorks;
