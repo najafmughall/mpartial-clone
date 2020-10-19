aa06385498601ee0c35fb7bcd5dcc9ed8d48c876
Najaf <najafali2002@gmail.com>
2020-10-19 19:19:42 +0500

    sketches

--- src/components/example-deliverables.jsx
+++ src/components/example-deliverables.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import img from "../images/img.jpg";
+import sktech1 from "../images/sketch-1.png";
+import sktech2 from "../images/sketch-2.png";
 
 class ExampleDeliverables extends Component {
   render() {
@@ -39,6 +42,106 @@ class ExampleDeliverables extends Component {
                   frameborder="0"
                 ></iframe>
               </div>
+            </div>
+            <br></br>
+            <div className="doc-slider">
+              <h3 className="widget-title">Immaculate. Impartial. [ESX]</h3>
+              <div className="carosuel slide">
+                <ol class="carousel-indicators">
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="2"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                    class="active"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img class="d-block w-100" src={img} alt="First slide" />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      className="d-block w-100"
+                      src={img}
+                      alt="Second slide"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      className="d-block w-100"
+                      src={img}
+                      alt="Third slide"
+                    />
+                  </div>
+                  <div className="carousel-item active">
+                    <img class="d-block w-100" src={img} alt="First slide" />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      className="d-block w-100"
+                      src={img}
+                      alt="Second slide"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      className="d-block w-100"
+                      src={img}
+                      alt="Third slide"
+                    />
+                  </div>
+                </div>
+                <a
+                  class="carousel-control-prev"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a
+                  class="carousel-control-next"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+            <div className="sketch-widget">
+              <h3 className="widget-title">TrueSketch PLUS [SKX]</h3>
+              <section className="sketch-container">
+                <img src={sktech1} alt="Sketch1" />
+                <img src={sktech2} alt="Sketch2" />
+              </section>
             </div>
           </div>
         </div>
